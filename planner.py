d195891cf527dad5f1d85bfeea53e79caedb69c1
Weicong Kuang <leokwong@utexas.edu>
2023-04-14 19:22:54 -0500

    Working with folium, move forward to streamlit

--- planner.py
+++ planner.py
@@ -1,6 +1,63 @@
-from getPOI import point_of_interest
+import folium
+import openrouteservice
+from geopy.geocoders import Nominatim
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import Point, LineString
+from shapely.ops import nearest_points
 
-KEY = 'AIzaSyA1_GpZ_vaKI7PsUB0Mx-cMtL_1BMyUncg'
-POI = point_of_interest(KEY)
-tourist_attractions_gdf = POI.get_tourist_attractions()
-national_parks = POI.get_national_parks()
+# Define the starting point, destination point, and attractions
+geolocator = Nominatim(user_agent="myGeocoder")
+starting_point = geolocator.geocode('Austin, TX')
+destination_point = geolocator.geocode('Richmond, CA')
+
+tourist_attractions_gdf = gpd.read_file('Tourist_Attractions.geojson')
+national_parks = gpd.read_file('National_Parks.geojson')
+attractions_gdf = pd.concat([tourist_attractions_gdf, national_parks])
+
+# Replace 'your_api_key' with your actual OpenRouteService API key
+client = openrouteservice.Client(key='5b3ce3597851110001cf624855194dc0c63a4c6c9d6db25d7fcb45b6')
+
+# Define the route coordinates
+coordinates = [
+    (starting_point.longitude, starting_point.latitude),
+    (destination_point.longitude, destination_point.latitude)
+]
+
+# Get the route from the OpenRouteService API
+route = client.directions(
+    coordinates=coordinates,
+    profile='driving-car',
+    format='geojson'
+)
+
+# Visualize the route using Folium
+map_route = folium.Map(location=[(starting_point.latitude + destination_point.latitude) / 2, (starting_point.longitude + destination_point.longitude) / 2], zoom_start=5)
+
+folium.GeoJson(
+    route,
+    name='Route',
+    style_function=lambda x: {'color': 'blue', 'weight': 5}
+).add_to(map_route)
+
+# Define the distance threshold
+distance_threshold = 1
+
+# Create a LineString from the route
+route_line = LineString(route['features'][0]['geometry']['coordinates'])
+
+# Create a buffer zone around the route
+route_buffer = route_line.buffer(distance_threshold)
+
+# Filter attractions that fall within the buffer zone
+attractions_gdf["in_buffer"] = attractions_gdf.apply(lambda row: route_buffer.contains(row["geometry"]), axis=1)
+filtered_attractions = attractions_gdf[attractions_gdf["in_buffer"]]
+
+# Add filtered attractions as small green trees
+for index, row in filtered_attractions.iterrows():
+    folium.Marker(
+        location=(row.geometry.y, row.geometry.x),
+        icon=folium.Icon(color='green', icon='tree', prefix='fa')
+    ).add_to(map_route)
+
+map_route.save('route_map.html')
